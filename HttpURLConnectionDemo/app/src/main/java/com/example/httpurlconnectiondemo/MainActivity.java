cef41f1717890686b8c55048a80e7a51cac1eb71
zj970 <3060529292@qq.com>
2021-07-10 20:17:26 +0800

    修改bug，测试失败

--- HttpURLConnectionDemo/app/src/main/java/com/example/httpurlconnectiondemo/MainActivity.java
+++ HttpURLConnectionDemo/app/src/main/java/com/example/httpurlconnectiondemo/MainActivity.java
@@ -11,9 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-
 import org.json.JSONObject;
-import org.json.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -34,7 +32,7 @@ public class MainActivity extends Activity {
     TextView tipTV;
     //食乐网登录请求地址
     String path = "http://192.168.1.1";
-    Handler handler = new Handler() {
+    final Handler handler = new Handler() {
         @Override
         public void publish(LogRecord record) {
 
@@ -49,19 +47,20 @@ public class MainActivity extends Activity {
         public void close() throws SecurityException {
 
         }
-        public void handleMessage(Message msg){
-            if (msg.what == 1){
-                Intent intent = new Intent(MainActivity.this,LoginOkActivity.class);
+
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Intent intent = new Intent(MainActivity.this, LoginOkActivity.class);
                 String userInfo = String.valueOf(msg.obj);
                 JSONObject jo = (JSONObject) JSON.parse(userInfo);
                 String joData = jo.getString("data");
-                if ("".equals(joData) || "[]".equals(joData)){
+                if ("".equals(joData) || "[]".equals(joData)) {
                     tipTV.setText("用户名或密码错误，登录失败!");
-                }else{
-                    intent.putExtra("userInfo",userInfo);
+                } else {
+                    intent.putExtra("userInfo", userInfo);
                     startActivity(intent);
                 }
-            }else if (msg.what == 0){
+            } else if (msg.what == 0) {
                 tipTV.setText(String.valueOf(msg.obj));
             }
         }
@@ -82,33 +81,33 @@ public class MainActivity extends Activity {
                 final String pwd = pwdET.getText().toString();
                 userNameEF.setText("");
                 tipTV.setText("");
-                if (userName.equals("") || pwd.equals("")){
-                    Toast.makeText(MainActivity.this,"用户名和密码不能为空",Toast.LENGTH_SHORT).show();
+                if (userName.equals("") || pwd.equals("")) {
+                    Toast.makeText(MainActivity.this, "用户名和密码不能为空", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        try{
+                        try {
                             URL url = new URL(path);
-                            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                             connection.setRequestMethod("POST");//请求方式POST
                             connection.setDoOutput(true);//允许写入
                             connection.setDoInput(true);//允许读入
                             connection.setUseCaches(false);//不使用户缓存
                             connection.connect();
-                            String body = "Param={'where':'account="+userName+"and password ="+pwd+"'}";
-                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(),"UTF-8"));
+                            String body = "Param={'where':'account=" + userName + "and password =" + pwd + "'}";
+                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
                             writer.write(body);
                             writer.close();
                             int responseCode = connection.getResponseCode();
-                            if (responseCode == HttpURLConnection.HTTP_OK){
+                            if (responseCode == HttpURLConnection.HTTP_OK) {
                                 InputStream inputStream = connection.getInputStream();//将流转换为字符串
                                 String entityStr = is2String(inputStream);
-                                Message msg = Message.obtain(handler,1,entityStr);
+                                Message msg = Message.obtain(handler, 1, entityStr);
                                 msg.sendToTarget();
-                            }else {
-                                Message msg = Message.obtain(handler,0,"网络连接异常");
+                            } else {
+                                Message msg = Message.obtain(handler, 0, "网络连接异常");
                                 msg.sendToTarget();
                             }
                         } catch (MalformedURLException e) {
@@ -126,17 +125,16 @@ public class MainActivity extends Activity {
             }
         });
     }
-    public String is2String(InputStream is) throws IOException{
+
+    public String is2String(InputStream is) throws IOException {
         //连接后，创建一个输入流来读取response内容
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is,"utf-8"));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "utf-8"));
         String line = "";
         StringBuilder stringBuilder = new StringBuilder();
         //每次读取一行，若非空则添加至stringBuilder
-        while ((line = bufferedReader.readLine()) != null){
+        while ((line = bufferedReader.readLine()) != null)
             stringBuilder.append(line);
             String result = stringBuilder.toString().trim();
-            return  result;
-        }
+            return result;
     }
-
 }
