fbf0dc2a8429abecc809babbd288f51b1f7c1de8
zj970 <3060529292@qq.com>
2022-07-07 20:38:03 +0800

    [add] [Update] UIWidgetTest/* [Show] TextView,EditView,Button,ProgressBar,ImageView的使用

--- StudyFirstAndroid/UIWidgetTest/README.md
+++ StudyFirstAndroid/UIWidgetTest/README.md
@@ -60,4 +60,109 @@ Button是程序用于和用户进行交互的一个重要控件。
             android:text="Button"
             android:textAllCaps="false"/>
 ```
-由于系统会对Button中的所有英文字母自动进行大写转换
+由于系统会对Button中的所有英文字母自动进行大写转换，可以使用 android:textAllCaps="false" 来禁用到默认特性
+
+## 1.3 EditText
+
+EditText是程序用于和用户进行交互的另一个重要的控件，它允许用户在控件里输入和编辑内容，并可以在程序中对这些内容进行处理。EditText的应用场景非常普遍，在进行发短信、发微信、发微博等操作时，你不得不使用EditText。
+
+```xml
+    <EditText
+            android:id="@+id/edit_text"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"/>
+```
+
+Android控件的使用规律：给控件定义一个id，再指定控件的高度和宽度，然后再适当加入一些控件特有的属性
+
+增加EditText提示功能 android:hint="Type something here"，当我们输入任何内容时，该提示文本就会消失
+
+```xml
+<EditText
+            android:id="@+id/edit_text"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:hint="Type something there"/>
+```
+
+&emsp;不过，随着输入的内容不断增多，EditText会被不断地拉长，这时由于EditText的高度是指定的是wrap_content,因此它总能包含住里面的内容，但是输入内容过多时，界面就变得非常难看
+
+我们可以使用 android:maxLines 属性来解决这个问题
+
+```xml
+<EditText
+            android:id="@+id/edit_text"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:hint="Type something there"
+            android:maxLines="2"/>
+```
+这里通过android:maxLines指定了EditText的最大行数为两行，当输入的内容超过两行时，文本就会向上滚动，而EditText则不会继续拉伸
+
+## 1.4 ImageView
+
+&emsp;&emsp;ImageView 是用于在界面上展示图片的一个控件，它可以让我们的程序界面变得更加丰富多彩。  
+学习这个控件需要提前准备一些图片，这些图片通常放在 ”drawable“ 开头的目录下的。
+
+```xml
+<ImageView
+            android:id="@+id/image_1"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:src="@drawable/img_1"/>
+```
+&emsp;&emsp;可以看到，这里使用android:src属性给ImageView指定了一张图片。由于图片的宽和高都是未知的，所有将ImageView的宽和高都设定了wrap_content，这样就保证了不管图片的尺寸是多少，图片都可以完整地显示出来。
+
+## 1.5 ProgressBar
+
+&emsp;emsp;ProgressBar用于在界面上显示一个进度条，表示我们的程序正在加载一些数据。
+
+```xml
+ <ProgressBar
+            android:id="@+id/progress_bar"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"/>
+
+```
+
+&emsp;&emsp;旋转的进度条表明我们的程序正在加载数据，那么数据总有加载完的时候。此时需要Android控件的可见属性。  
+所有的Android控件都具有这个属性，可以通过android:visibility进行指定，可选值有3种：
+- visible 表示控件是可见的，这个值是默认值，不指定的时候就是可见的
+- invisible 表示控件不可见，但它仍然占据原来的位置和大小，只是变为透明状态了。
+- gone 不仅不可见，而且不再占用任何屏幕空间
+
+我们还可以通过代码来设置控件的可见性，使用的是setVisibility()方法，可以传入View.VISIBLE、View.INVISIBLE和View.GONE这三种值  
+另外我们还可以给ProgressBar指定不同的样式，通过style属性可以将它指定成水平进度条
+
+```xml
+<ProgressBar
+            android:id="@+id/progress_bar"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            style="?android:attr/progressBarStyleHorizontal"
+            android:max="100"/>
+```
+## 1.6 AlertDialog
+
+AlertDialog 可以在当前的界面弹出一个对话框，这个对话框是置顶于所有界面元素之上的，能够屏蔽掉其他控件的交互能力，因此 AlertDialog 一般都是用于提示一些非常重要的内容或者警告信息。比如为了防止用户误删重要内容，在删除之前弹出一个确认对话框
+
+```
+AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("This is Dialog");
+        dialog.setMessage("Something important.");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
+```
+首先通过AlertDialog.Builder创建一个AlertDialog实例，然后可以为这个对话框设置标题、内容、可否取消等属性，接下来调用setPositiveButton()方法为对话框设置确定点击按钮的点击事件，调用setNegativeButton()方法设置取消按钮点击事件，最后调用show()方法将对话框显示出来
--- StudyFirstAndroid/UIWidgetTest/app/src/main/java/com/zj970/uiwidgettest/MainActivity.java
+++ StudyFirstAndroid/UIWidgetTest/app/src/main/java/com/zj970/uiwidgettest/MainActivity.java
@@ -1,13 +1,121 @@
 package com.zj970.uiwidgettest;
 
+import android.content.DialogInterface;
+import android.os.Process;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button button;
+    private Button buttonChangeImage;
+    private TextView text_view;
+    private EditText edit_text;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+    boolean isImage_1 = true;//默认是img_1
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //先加载资源文件再实例化
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this::onClick);//不使用匿名类的方式，采取继承 View.OnClickListener 接口的方式
+
+        buttonChangeImage = findViewById(R.id.changeImage);
+        buttonChangeImage.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.button:
+                //TODO： 实现button的响应事件
+                button_OnClick();
+                break;
+
+            case R.id.changeImage:
+                //TODO: 实现button_ChangeImage的响应事件
+                button_ChangeImage();
+            default:
+                break;
+        }
+    }
+
+    private void button_OnClick(){
+        //TODO: 实现点击button按钮将输入框的内容转换为标签的内容
+
+        /**
+         * 1. 首先通过findViewById()的方法得到 text_view 和 edit_text的实例
+         * 2. 然后调用 EditText.getText()方法获取输入到的内容
+         * 3. 对TextView.setText()进行赋值
+         * 4. 通过Toast 将输入的内容显示出来
+         */
+        text_view = findViewById(R.id.text_view);
+        edit_text = findViewById(R.id.edit_text);
+        String edit_text_toString =edit_text.getText().toString();
+        Log.d(TAG,edit_text_toString);
+        text_view.setText(edit_text.getText());
+        //Toast ---> 消息
+        Toast.makeText(MainActivity.this, edit_text_toString, Toast.LENGTH_LONG).show();
+        //----<弹窗
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);//1. 实例化AlertDialog
+        dialog.setTitle("This is Dialog");//2. 设置标题
+        dialog.setMessage("Something important.");//3. 设置内容
+        dialog.setCancelable(false);//4. 可否取消的属性
+        /**
+         * 确定按钮点击事件
+         */
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        /**
+         * 取消按钮点击事件
+         */
+        dialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
+    }
+
+    private void button_ChangeImage(){
+        //TODO: 实现点击button_change_image 按钮将图片img_1 和 img_2 替换
+        imageView = findViewById(R.id.image_view);
+        //TODO: 设置点击时长，加载时长  -- 定时器 未实现效果
+
+        progressBar = findViewById(R.id.progress_bar);
+        /*if (progressBar.getVisibility() == View.GONE){
+            progressBar.setVisibility(View.VISIBLE);
+        }else {
+            progressBar.setVisibility(View.GONE);
+        }*/
+        int progress = progressBar.getProgress();
+        progress +=10;
+        progressBar.setProgress(progress);
+
+        if (isImage_1){
+            imageView.setImageResource(R.drawable.img_2);
+            isImage_1 = false;
+        } else {
+            imageView.setImageResource(R.drawable.img_1);
+            isImage_1 = true;
+        }
     }
 }
