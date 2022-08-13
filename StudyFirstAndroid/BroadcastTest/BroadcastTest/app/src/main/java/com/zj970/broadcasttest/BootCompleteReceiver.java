a09547f97dcb31c7ef9f0fb6546beeb4e3f64485
zj970 <3060529292@qq.com>
2022-08-13 20:24:57 +0800

    [Add] BroadcastTest/* [Show] 初识广播以及广播的类型、注册。标准广播、有序广播；动态注册和静态注册

---
+++ StudyFirstAndroid/BroadcastTest/BroadcastTest/app/src/main/java/com/zj970/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,19 @@
+package com.zj970.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        Toast.makeText(context,"Boot Complete",Toast.LENGTH_LONG).show();
+        Log.d("zj970", "onReceive: 开机");
+    }
+}
