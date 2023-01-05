a911b4d431c6fe85887f299cb897c65f066c3652
zj970 <3060529292@qq.com>
2023-01-05 18:40:37 +0800

    [Module] 旅行食记App [Show] 添加探索页面以及部分字符串string化

---
+++ AndroidStudy/Tourism/app/src/main/java/com/zj970/tourism/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.zj970.tourism;
+
+import android.os.Bundle;
+import com.zj970.tourism.base.BaseActivity;
+
+/**
+ * @author zj970
+ */
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
