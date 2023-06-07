3c4eb94a181eac4cd40d62cccd56e98620e6ce40
zj970 <3060529292@qq.com>
2023-06-07 14:05:33 +0800

    Android 天气APP(十七) 重新构建项目

---
+++ AndroidStudy/GoodWeather/app/src/main/java/com/example/goodweather/ui/CommonlyUsedCityActivity.java
@@ -0,0 +1,21 @@
+package com.example.goodweather.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.goodweather.databinding.ActivityCommonlyUsedCityBinding;
+import com.example.goodweather.databinding.ActivityManageCityBinding;
+import com.example.mylibrary.base.NetworkActivity;
+
+public class CommonlyUsedCityActivity extends NetworkActivity<ActivityCommonlyUsedCityBinding> {
+
+
+    @Override
+    protected void onCreate() {
+
+    }
+
+    @Override
+    protected void onObserveData() {
+
+    }
+}
